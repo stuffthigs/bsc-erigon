019b01f49c063bda55891a89bee3fa194b3db132
blxdyx <blxdyx@outlook.com>
2025-01-22 13:50:18 +0800

    upstream beta-1

--- params/version.go
+++ params/version.go
@@ -34,9 +34,9 @@ var (
 
 // see https://calver.org
 const (
-	VersionMajor       = 1        // Major version component of the current release
-	VersionMinor       = 3        // Minor version component of the current release
-	VersionMicro       = 0        // Patch version component of the current release
+	VersionMajor       = 1       // Major version component of the current release
+	VersionMinor       = 3       // Minor version component of the current release
+	VersionMicro       = 0       // Patch version component of the current release
 	VersionModifier    = "beta1" // Modifier component of the current release
 	VersionKeyCreated  = "ErigonVersionCreated"
 	VersionKeyFinished = "ErigonVersionFinished"
