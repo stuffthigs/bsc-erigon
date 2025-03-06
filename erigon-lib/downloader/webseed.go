21c7394506b3813c97c1d86d25ffde67388cdb2f
blxdyx <125243069+blxdyx@users.noreply.github.com>
2025-03-06 11:09:18 +0800

    Revert "StageSnapshot struck: fix "no metadata yet" (#621)" (#635)
    This reverts commit 1ad66a5ec3a1d18fa29a6996d57591ccdbba247c.

--- erigon-lib/downloader/webseed.go
+++ erigon-lib/downloader/webseed.go
@@ -645,7 +645,7 @@ func (d *WebSeeds) DownloadAndSaveTorrentFile(ctx context.Context, name string) 
 			continue // it's ok if some HTTP provider failed - try next one
 		}
 		res, err := d.callTorrentHttpProvider(ctx, parsedUrl, name)
-		if err != nil || res == nil {
+		if err != nil {
 			d.logger.Debug("[snapshots] .torrent from webseed rejected", "name", name, "err", err, "url", urlStr)
 			continue // it's ok if some HTTP provider failed - try next one
 		}
@@ -670,7 +670,7 @@ func (d *WebSeeds) callTorrentHttpProvider(ctx context.Context, url *url.URL, fi
 	request = request.WithContext(ctx)
 	resp, err := d.client.Do(request)
 	if err != nil {
-		return nil, nil
+		return nil, fmt.Errorf("webseed.downloadTorrentFile: url=%s, %w", url.String(), err)
 	}
 	defer resp.Body.Close()
 	//protect against too small and too big data
